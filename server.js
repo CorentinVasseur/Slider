3be4552a8c6eb2852d2229ac57d30f48e6b50519
Corentin <vasseur.corentin@gmail.com>
2016-08-27 17:17:37 +0200

    specify babel presets

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config')
 
-var app = new (require('express'))()
-var port = 8888
+const app = new (require('express'))()
+const port = 8888
 
-var compiler = webpack(config)
+const compiler = webpack(config)
 
-var initialState = require('./common/initialState.json')
+const initialState = require('./common/initialState.json')
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,3 @@
-/*var path = require('path')
-var webpack = require('webpack')
-
-module.exports = {
-	devtol: 'cheap-module-eval-source-map',
-	entry: [
-		'webpack-hot-middleware/client',
-		'./index'
-	],
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/static/'
-	},
-  	plugins: [
-   	 	new webpack.optimize.OccurrenceOrderPlugin(),
-    	new webpack.HotModuleReplacementPlugin()
-  	],
-	module: {
-    	loaders: [
-      	{
-        	test: /\.js$/,
-        	loaders: [ 'babel' ],
-        	exclude: /node_modules/,
-        	include: __dirname
-      	}
-      ]
-	}
-}*/
-
 var path = require('path')
 var webpack = require('webpack')
 
@@ -51,11 +21,11 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: [ 'babel'],
+        loaders: [ 'babel','babel?presets[]=react,presets[]=es2015'],
         exclude: /node_modules/,
         include: __dirname
       },
-      
+
       { test: /\.css$/, loader: 'style!css'},
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
       { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
